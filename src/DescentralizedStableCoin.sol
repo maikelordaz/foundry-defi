0f47db7182ce5e9464f59149ac9636a9df7adb1d
Maikel Ordaz <maikelordaz@gmail.com>
2024-03-21 18:58:33 -0400

    constructor

--- src/DescentralizedStableCoin.sol
+++ src/DescentralizedStableCoin.sol
@@ -2,6 +2,8 @@
 
 pragma solidity 0.8.19;
 
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
 /**
  * @title DescentralizedStableCoin
  * @author Maikel Ordaz
@@ -10,6 +12,6 @@ pragma solidity 0.8.19;
  * @notice Stability: Pegged to the US Dollar
  * @dev This contract is governed by DSCEngine contract, this is just the ERC20 token implementation contract
  */
-contract DescentralizedStableCoin {
-    constructor() {}
+contract DescentralizedStableCoin is ERC20Burnable {
+    constructor() ERC20("DescentralizedStableCoin", "DSC") {}
 }
