c11c75b3602a0d96e09d4c787e12e121ab40be84
Maikel Ordaz <maikelordaz@gmail.com>
2024-03-21 18:44:45 -0400

    contract description

---
+++ src/DescentralizedStableCoin.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+
+/**
+ * @title DescentralizedStableCoin
+ * @author Maikel Ordaz
+ * @notice Collateral: Exogenous (ETH & BTC)
+ * @notice Minting: Algorithmic
+ * @notice Stability: Pegged to the US Dollar
+ * @dev This contract is governed by DSCEngine contract, this is just the ERC20 token implementation contract
+ */
+contract DescentralizedStableCoin {}
