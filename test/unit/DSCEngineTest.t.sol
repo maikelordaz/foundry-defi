6c8c4480f43418f8af6130f2521db78f74a5ac28
Maikel Ordaz <maikelordaz@gmail.com>
2024-03-24 18:24:31 -0400

    initial tests

---
+++ test/unit/DSCEngineTest.t.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployDSC} from "../../scripts/DeployDSC.s.sol";
+import {DescentralizedStableCoin} from "../../src/DescentralizedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {HelperConfig} from "../../scripts/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+
+contract DSCEngineTest is Test {
+    DeployDSC deployer;
+    DescentralizedStableCoin dsc;
+    DSCEngine engine;
+    HelperConfig config;
+    address ethUsdPriceFeed;
+    address weth;
+
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
+    function setUp() public {
+        deployer = new DeployDSC();
+        (dsc, engine, config) = deployer.run();
+        (ethUsdPriceFeed, , weth, , ) = config.activeNetworkConfig();
+
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
+    }
+
+    /*************************************** Price feed tests **********************************/
+    function testGetUsdValue() public view {
+        uint256 ethAmount = 15e18;
+        // 15e18 * 2000/ETH = 30000e18
+
+        uint256 expectedUsdValue = 30000e18;
+        uint256 actualUsdValue = engine.getUsdValue(weth, ethAmount);
+
+        assertEq(actualUsdValue, expectedUsdValue);
+    }
+
+    /*************************************** Deposit Collateral tests **********************************/
+
+    function testRevertIfCollateralZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(engine), AMOUNT_COLLATERAL);
+
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        engine.depositCollateral(weth, 0);
+
+        vm.stopPrank();
+    }
+}
