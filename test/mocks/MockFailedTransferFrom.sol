342f3ca4b1bd6af348de3fb760f73204206d03a5
Maikel Ordaz <maikelordaz@gmail.com>
2024-04-08 21:11:08 -0400

    change pragma

--- test/mocks/MockFailedTransferFrom.sol
+++ test/mocks/MockFailedTransferFrom.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.24;
 
 import {ERC20Burnable, ERC20} from "openzeppelin/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "openzeppelin/access/Ownable.sol";
